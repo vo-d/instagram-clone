d8dd07b39a847a1584b8b70dac9aa50d0b2ff19e
vo-d <daidaivo2002@gmail.com>
2023-01-30 22:32:41 -0600

    header

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Image from 'next/image'
+import {MagnifyingGlassIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, Bars3Icon} from '@heroicons/react/24/outline'
+
+function Header() {
+  return (
+    <div>
+      <div className='flex justify-between max-w-6xl'>
+        {/* Left */}
+        <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
+          <Image src="https://links.papareact.com/ocw" fill style={{objectFit:"contain"}}></Image>
+        </div>
+
+        <div className='relative lg:hidden w-10 flex-shrink-0 cursor-pointer'>
+          <Image src="https://links.papareact.com/jjm" fill style={{objectFit:"contain"}}></Image>
+        </div>
+        {/* Middle -search input field*/}
+        <div className='max-w-xs'>
+          <div className='relative mt-1 p-3 rounded-md'>
+            <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+              <MagnifyingGlassIcon className='h-5 w-5 text-gray-500'></MagnifyingGlassIcon>
+            </div>
+            <input type="text" placeholder='Search' className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md'/>
+          </div>
+          
+        </div>
+
+        {/* Right */}
+      </div> 
+    </div>
+  )
+}
+
+export default Header
